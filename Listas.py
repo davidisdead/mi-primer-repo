ad1a5037bdd5159166c4be4d3a74420a1f03f4f5
David Hernández <d_h_h_03@comunidad.unam.mx>
2026-02-11 18:27:25 +0000

    Añadir funcion para devolver el elemento maximo

--- Listas.py
+++ Listas.py
@@ -1,3 +1,9 @@
-#Creación de la función para multiplicar cada elemento por 2
+#Función para multiplicar cada elemento por 2
 def double(arr):
     return [x * 2 for x in arr]
+
+#Función que devuelve el elemento máximo de un arreglo
+def maximum(arr):
+    if len(arr) == 0:
+        return None
+    return max(arr)
